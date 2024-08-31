553a11beac3f06e92e4330d2c01a752b2b2e0f80
Chiranjeevi Karthik Kuruganti <karthikchiru12@gmail.com>
2024-09-01 05:19:22 +0530

    Added more custom transforms
    1. abs
    2. ceil
    3. floor
    4. round
    5. random
    6. slugify
    7. unslugify

--- core/custom_transforms.ts
+++ core/custom_transforms.ts
@@ -683,4 +683,128 @@ return {
 };
 });
 
+mozjexl.addTransform("abs", (val: Array<number> | number) => {
+  if (typeof val === "number") return Math.abs(val);
+  const result: Array<number | ErrorObject> = [];
+  if (typeof val === "object" && Array.isArray(val)) {
+    val.forEach((record, idx) => {
+      if (typeof record === "number") {
+        result.push(Math.abs(record));
+      } else {
+        result.push({
+          "error-103": `The value ${record} of type ${getType(record)} at index ${idx} has no method 'abs'. <value> | abs is only supported for Number, Array<Number>`
+        });
+      }
+    });
+    return result;
+  }
+  return {
+    "error-103": `The ${val} of type ${getType(val)} has no method 'abs'. <value> | abs is only supported for Number, Array<Number>`
+  };
+});
+
+mozjexl.addTransform("ceil", (val: Array<number> | number) => {
+  if (typeof val === "number") return Math.ceil(val);
+  const result: Array<number | ErrorObject> = [];
+  if (typeof val === "object" && Array.isArray(val)) {
+    val.forEach((record, idx) => {
+      if (typeof record === "number") {
+        result.push(Math.ceil(record));
+      } else {
+        result.push({
+          "error-103": `The value ${record} of type ${getType(record)} at index ${idx} has no method 'ceil'. <value> | ceil is only supported for Number, Array<Number>`
+        });
+      }
+    });
+    return result;
+  }
+  return {
+    "error-103": `The ${val} of type ${getType(val)} has no method 'ceil'. <value> | ceil is only supported for Number, Array<Number>`
+  };
+});
+
+mozjexl.addTransform("floor", (val: Array<number> | number) => {
+  if (typeof val === "number") return Math.floor(val);
+  const result: Array<number | ErrorObject> = [];
+  if (typeof val === "object" && Array.isArray(val)) {
+    val.forEach((record, idx) => {
+      if (typeof record === "number") {
+        result.push(Math.floor(record));
+      } else {
+        result.push({
+          "error-103": `The value ${record} of type ${getType(record)} at index ${idx} has no method 'floor'. <value> | floor is only supported for Number, Array<Number>`
+        });
+      }
+    });
+    return result;
+  }
+  return {
+    "error-103": `The ${val} of type ${getType(val)} has no method 'floor'. <value> | floor is only supported for Number, Array<Number>`
+  };
+});
+
+mozjexl.addTransform("round", (val: Array<number> | number) => {
+  if (typeof val === "number") return Math.round(val);
+  const result: Array<number | ErrorObject> = [];
+  if (typeof val === "object" && Array.isArray(val)) {
+    val.forEach((record, idx) => {
+      if (typeof record === "number") {
+        result.push(Math.round(record));
+      } else {
+        result.push({
+          "error-103": `The value ${record} of type ${getType(record)} at index ${idx} has no method 'round'. <value> | round is only supported for Number, Array<Number>`
+        });
+      }
+    });
+    return result;
+  }
+  return {
+    "error-103": `The ${val} of type ${getType(val)} has no method 'round'. <value> | round is only supported for Number, Array<Number>`
+  };
+});
+
+mozjexl.addTransform("random", (_val: null) => {
+  return Math.random();
+});
+
+mozjexl.addTransform("slugify", (val: Array<string> | string) => {
+  if (typeof val === "string") return encodeURI(val);
+  const result: Array<string | ErrorObject> = [];
+  if (typeof val === "object" && Array.isArray(val)) {
+    val.forEach((record, idx) => {
+      if (typeof record === "string") {
+        result.push(encodeURI(record));
+      } else {
+        result.push({
+          "error-103": `The value ${record} of type ${getType(record)} at index ${idx} has no method 'slugify'. <value> | slugify is only supported for String, Array<String>`
+        });
+      }
+    });
+    return result;
+  }
+  return {
+    "error-103": `The ${val} of type ${getType(val)} has no method 'slugify'. <value> | slugify is only supported for String, Array<String>`
+  };
+});
+
+mozjexl.addTransform("unslugify", (val: Array<string> | string) => {
+  if (typeof val === "string") return decodeURI(val);
+  const result: Array<string | ErrorObject> = [];
+  if (typeof val === "object" && Array.isArray(val)) {
+    val.forEach((record, idx) => {
+      if (typeof record === "string") {
+        result.push(decodeURI(record));
+      } else {
+        result.push({
+          "error-103": `The value ${record} of type ${getType(record)} at index ${idx} has no method 'unslugify'. <value> | unslugify is only supported for String, Array<String>`
+        });
+      }
+    });
+    return result;
+  }
+  return {
+    "error-103": `The ${val} of type ${getType(val)} has no method 'unslugify'. <value> | unslugify is only supported for String, Array<String>`
+  };
+});
+
 export default mozjexl;
